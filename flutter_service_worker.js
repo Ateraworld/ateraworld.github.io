ef8e347a785755661353bb46f6a0755b1aecdbd5
Matteo Beltrame <bltmtt@gmail.com>
2023-03-04 23:21:37 +0100

    Test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,9 +58,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7b1daf7ee7dcebcc9682a1120e45e1b6",
-"/": "7b1daf7ee7dcebcc9682a1120e45e1b6",
-"main.dart.js": "104eabb595f16aea8e63066922bfd29b",
+"index.html": "2c97e8c134a0ee17fcea91a274a55c5f",
+"/": "2c97e8c134a0ee17fcea91a274a55c5f",
+"main.dart.js": "7bd175e26521e3ea2e39d60265ac6fc9",
 "manifest.json": "885dd1d1b82a940158b57d669a2e466f",
 "version.json": "5d2becf7d3b503dcfda59c10faa57e37"
 };
